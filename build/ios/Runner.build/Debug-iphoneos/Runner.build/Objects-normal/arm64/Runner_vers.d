586d30f57ba1ee906d6666ae060fd8e46b8e0326
BahadirIren <bahadiriren@gmail.com>
2021-03-13 00:41:58 +0300

    project files added

---
+++ build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/build/ios/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
---
+++ build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/main.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Runner/main.m \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Pods/Headers/Public/path_provider/path_provider.modulemap \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Flutter/Flutter.framework/Headers/Flutter.h \
+  /Users/bahadir/Developer/projeler/xylophone-flutter/ios/Runner/AppDelegate.h
